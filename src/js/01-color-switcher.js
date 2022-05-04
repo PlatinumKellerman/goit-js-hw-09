7600fcd17cb139ddf7de320a30dbb0608b232d04
PlatinumKellerman <platinumm@ukr.net>
2022-05-04 19:35:45 +0300

    homework-09-in-progress

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+const refs = {
+    body: document.querySelector('body'),
+    startButton: document.querySelector('button[data-start]'),
+    stopButton: document.querySelector('button[data-stop]')
+}
 
+refs.startButton.addEventListener('click', getRandomHexColor)
+
+
+function qqq() {
+    refs.body.style.background
+}
+
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
