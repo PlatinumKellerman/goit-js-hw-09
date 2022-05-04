64a2732958aff933a13a7f2d6ef93c50756cd1c0
PlatinumKellerman <platinumm@ukr.net>
2022-05-05 00:10:01 +0300

    task-02-in-progress

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,3 +1,24 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
+const refs = {
+    input: document.querySelector('#datetime-picker'),
+    startButton: document.querySelector('button[data-start]'),
+}
+
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+        onClose(selectedDates) {
+            const selectedDate = new Date(selectedDates[0]);
+            const currentDate = new Date();
+            const deltaDate = selectedDate - currentDate;
+            console.log(deltaDate);
+    },
+};
+
+flatpickr(refs.input, options);
+
