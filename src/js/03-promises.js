a1bd8804646477ae73cc0bc3d0fe165adacb9f16
PlatinumKellerman <platinumm@ukr.net>
2022-05-06 21:28:38 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,3 +6,26 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+
+console.log('B');
+
+
+
+
+console.log('C');
+
+const promise = new Promise((resolve, reject) => {
+let a = 0;
+
+for (let i = 0; i < 1000000000; i += 1) {
+  a += i;
+}
+  resolve(a);
+})
+
+promise.then((result) => {
+  return console.log('result', result);
+}).then((qwe) => {
+return console.log(qwe);
+})
